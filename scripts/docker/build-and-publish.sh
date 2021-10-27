b12a72447e762520098670fe0e534731a0af0b77
Sergey Lubimov <siteup24@gmail.com>
2021-10-27 13:06:04 +0300

    refactor: change to node:12.20-buster

--- scripts/docker/build-and-publish.sh
+++ scripts/docker/build-and-publish.sh
@@ -4,9 +4,9 @@ REGION=eu-central-1
 AWS_ACCOUNT_ID=914773708367
 PROFILE=default
 #PROFILE=personalAccount
-DOCKER_APP_NAME=total-new-app
+DOCKER_APP_NAME=selub-cart-api
 
-DOCKER_APP_REPOSITORY=total-new-app
+DOCKER_APP_REPOSITORY=selub-cart-api
 AWS_DOCKER_REGISTRY_URL=$AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 DOCKER_TIME_TAG="$(date +%s)"
 DOCKER_LATEST_TAG=latest
